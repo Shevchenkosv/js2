a872291e9fdf13ff34d885f4e5beed5887d03823
Shevchenkosv <85148865+Shevchenkosv@users.noreply.github.com>
2021-11-22 22:46:44 +0300

    Add files via upload
    д/з урок 5

---
+++ lesson5/js/main.js
@@ -0,0 +1,52 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+        show: false,
+        catalogUrl: '/catalogData.json',
+        cartItems: [],
+        filtered: [],
+        products: [],
+        imgProduct: 'https://via.placeholder.com/200x150'
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => console.log(error))
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+        },
+        filter() {
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    mounted() {
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents) {
+                    this.cartItems.push(item);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data) {
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for (let item of data) {
+                    this.products.push(item);
+                    this.filtered.push(item);
+                }
+            })
+    }
+
+});
+
